803e82c1357f90f9f97f7397d79243245bfa2376
ruan65 <ruan65@gmail.com>
2015-04-20 18:26:12 +0300

    info card view added

--- app/src/main/java/com/engstuff/coloriphornia/activities/FullScreenColorC.java
+++ app/src/main/java/com/engstuff/coloriphornia/activities/FullScreenColorC.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.engstuff.coloriphornia.R;
@@ -17,24 +20,29 @@ import com.engstuff.coloriphornia.helpers.ColorParams;
 
 public class FullScreenColorC extends Activity implements OnClickListener {
 
+    private CardView cv;
     private TextView tv;
-    protected LinearLayout container;
+    protected RelativeLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_color_c);
 
-        container = (LinearLayout) findViewById(R.id.layout_full_screen_color);
+        container = (RelativeLayout) findViewById(R.id.layout_full_screen_color);
 
         Intent intent = getIntent();
 
         String hexString = intent.getStringExtra(Cv.EXTRA_MESSAGE_COLOR_1);
 
         int backColor = (int) Long.parseLong(hexString.substring(1), 16);
+        int backCardColor = (int) Long.parseLong(hexString.substring(3), 16);
 
         container.setBackgroundColor(backColor);
         container.setOnClickListener(this);
+
+        cv = (CardView) findViewById(R.id.card_view_full_c);
+        cv.setCardBackgroundColor(backCardColor);
 
         tv = (TextView) findViewById(R.id.tv_color_params);
 
@@ -42,6 +50,7 @@ public class FullScreenColorC extends Activity implements OnClickListener {
                 ? Color.WHITE
                 : Color.BLACK;
 
+        tv.setBackgroundColor(backCardColor);
         tv.setTextColor(textColor);
         tv.setText(ColorParams.composeInfo(hexString));
     }
