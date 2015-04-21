16105a8329e4060225b7a81ee010c9b1083d91fd
ruan65 <ruan65@gmail.com>
2015-04-21 11:01:08 +0300

    full scren color_cc fragmentated

--- app/src/main/java/com/engstuff/coloriphornia/activities/FullScreenColorCC.java
+++ app/src/main/java/com/engstuff/coloriphornia/activities/FullScreenColorCC.java
@@ -1,68 +1,40 @@
 package com.engstuff.coloriphornia.activities;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.engstuff.coloriphornia.R;
 import com.engstuff.coloriphornia.data.Cv;
-import com.engstuff.coloriphornia.helpers.ColorParams;
+import com.engstuff.coloriphornia.fragments.FullScreenColorFragment;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-
-
-public class FullScreenColorCC extends FullScreenColorC {
-
-    @InjectView(R.id.tv_color_params_1) TextView tv1;
-    @InjectView(R.id.tv_color_params_2) TextView tv2;
-
-    @InjectView(R.id.layout_full_screen_color_1) RelativeLayout rl1;
-    @InjectView(R.id.layout_full_screen_color_2) RelativeLayout rl2;
+public class FullScreenColorCC extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_color_cc);
 
-        ButterKnife.inject(this);
-
         Intent intent = getIntent();
 
-//        rl1.setOnClickListener(this);
-//        rl2.setOnClickListener(this);
+        FullScreenColorFragment fragment1 = new FullScreenColorFragment();
+        FullScreenColorFragment fragment2 = new FullScreenColorFragment();
 
-        // set color 1
-        setColor(intent,
-                Cv.EXTRA_MESSAGE_COLOR_1, Cv.EXTRA_MESSAGE_TEXT_COLOR_1,
-                rl1, tv1);
+        fragment1.setHexString(intent.getStringExtra(Cv.EXTRA_MESSAGE_COLOR_1));
+        fragment2.setHexString(intent.getStringExtra(Cv.EXTRA_MESSAGE_COLOR_2));
 
-        // set color 2
-        setColor(intent,
-                Cv.EXTRA_MESSAGE_COLOR_2, Cv.EXTRA_MESSAGE_TEXT_COLOR_2,
-                rl2, tv2);
+        fragment1.setWhiteText(intent.getBooleanExtra(Cv.EXTRA_MESSAGE_TEXT_COLOR_1, false));
+        fragment2.setWhiteText(intent.getBooleanExtra(Cv.EXTRA_MESSAGE_TEXT_COLOR_2, false));
+        
+        getFragmentManager().beginTransaction()
+                .add(R.id.frame_full_screen_color_1, fragment1)
+                .add(R.id.frame_full_screen_color_2, fragment2)
+                .commit();
     }
 
-    private void setColor(Intent intent, String extra1, String extra2,
-                          RelativeLayout rl, TextView tv) {
-
-        String hexColor = intent.getStringExtra(extra1);
-
-        int backColor = (int) Long.parseLong(hexColor.substring(1), 16);
-
-        rl.setBackgroundColor(backColor);
-
-        int textColor1 =
-                intent.getBooleanExtra(extra2, false)
-                ? Color.WHITE
-                : Color.BLACK;
-
-        tv.setTextColor(textColor1);
-        tv.setText(ColorParams.composeInfo(hexColor));
+    @Override
+    protected void onResume() {
+        super.onResume();
+        overridePendingTransition(R.anim.slide_in_l, R.anim.slide_out_l);
     }
 }
