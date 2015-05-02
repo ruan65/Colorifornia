e44c8d4e387cfedc91751ee878bc6a67e0298006
ruan65 <ruan65@gmail.com>
2015-05-02 22:13:53 +0300

    seek bar further steps

--- app/src/main/java/com/engstuff/coloriphornia/components/views/SeekBarMinusPlus.java
+++ app/src/main/java/com/engstuff/coloriphornia/components/views/SeekBarMinusPlus.java
@@ -2,20 +2,32 @@ package com.engstuff.coloriphornia.components.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+
+import com.engstuff.coloriphornia.R;
 
 public class SeekBarMinusPlus extends LinearLayout {
 
+    private SeekBar seekBar;
+    private SeekBarButton minus, plus;
+
     public SeekBarMinusPlus(Context context) {
         super(context);
+
+
     }
 
     public SeekBarMinusPlus(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        setOrientation(HORIZONTAL);
+        setGravity(Gravity.CENTER_VERTICAL);
+
+        ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                .inflate(R.layout.seek_bar_minus_plus, this, true);
     }
 
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
-    }
 }
