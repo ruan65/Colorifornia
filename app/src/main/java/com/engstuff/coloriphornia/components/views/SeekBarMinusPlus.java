4d9e19203acf5ce87f0ab377a6100f85ca269bac
ruan65 <ruan65@gmail.com>
2015-05-02 21:30:04 +0300

    custom seekBarMinusPlus class created

---
+++ app/src/main/java/com/engstuff/coloriphornia/components/views/SeekBarMinusPlus.java
@@ -0,0 +1,21 @@
+package com.engstuff.coloriphornia.components.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class SeekBarMinusPlus extends LinearLayout {
+
+    public SeekBarMinusPlus(Context context) {
+        super(context);
+    }
+
+    public SeekBarMinusPlus(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
